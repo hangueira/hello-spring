fe556d42fef96ed14e4aa7a716be25f8af267504
hangueira <hks713@naver.com>
2021-10-18 17:24:08 +0900

    start!! spring boot!

---
+++ src/test/java/com/hks/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hks.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
